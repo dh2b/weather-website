fea295c73c2a39474362fb76fa2d0938272045f0
Darcio Bertocco <darciohbb@gmail.com>
2019-04-16 22:14:52 -0300

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if ( body.error ) {
             callback( 'Unable to find location', undefined);
         } else {
-            callback( undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`);
+            callback( undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature}ºC degrees out. The high today is ${body.daily.data[0].temperatureHigh}ºC with a low of ${body.daily.data[0].temperatureLow}ºC. There is a ${body.currently.precipProbability}% chance of rain`);
         }
 
     });
